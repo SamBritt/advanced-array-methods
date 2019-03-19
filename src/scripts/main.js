997dfed20019a9b3a491b5376060e69f8071272b
SamBritt <sam.britt2@yahoo.com>
2019-03-19 14:35:33 -0500

    practice using forEach

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+const outEl = document.querySelector("#output")
+outEl.innerHTML = "<h1>Active Businesses</h1>"
+
+let zipCodeProperty = "addressZipCode";
+
+businesses.forEach(business => {
+    console.log(business);
+  outEl.innerHTML += `
+    <h2>${business.companyName}</h2>
+    <section>
+      ${business.addressFullStreet}
+    </section>
+    <section>
+    ${business.addressCity},
+    ${business["addressStateCode"]}
+    ${business[zipCodeProperty]}
+    </section>
+  `
+  outEl.innerHTML += "<hr/>"
+});
