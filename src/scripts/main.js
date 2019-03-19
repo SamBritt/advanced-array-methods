ee5d7302a2b88c87cda0483c955e2547288f21cc
SamBritt <sam.britt2@yahoo.com>
2019-03-19 15:05:01 -0500

    practice filter method'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,51 @@ outEl.innerHTML = "<h1>Active Businesses</h1>"
 
 let zipCodeProperty = "addressZipCode";
 
-businesses.forEach(business => {
-    console.log(business);
-  outEl.innerHTML += `
-    <h2>${business.companyName}</h2>
-    <section>
-      ${business.addressFullStreet}
-    </section>
-    <section>
-    ${business.addressCity},
-    ${business["addressStateCode"]}
-    ${business[zipCodeProperty]}
-    </section>
-  `
-  outEl.innerHTML += "<hr/>"
+const writeToDom = (arr) => {
+    arr.forEach(business => {
+        outEl.innerHTML += `
+            <h2>${business.companyName}</h2>
+            <section>
+              ${business.addressFullStreet}
+            </section>
+            <section>
+            ${business.addressCity},
+            ${business["addressStateCode"]}
+            ${business[zipCodeProperty]}
+            </section>
+          `
+        outEl.innerHTML += "<hr/>"
+    })
+}
+
+// businesses.forEach(business => {
+//     console.log(business);
+//   outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//     ${business.addressCity},
+//     ${business["addressStateCode"]}
+//     ${business[zipCodeProperty]}
+//     </section>
+//   `
+//   outEl.innerHTML += "<hr/>"
+// });
+
+const newYorkBusinesses = businesses.filter(business => {
+    let inNewYork = false
+
+    if (business.addressStateCode === "NY") {
+        inNewYork = true
+    }
+
+    return inNewYork
 });
+// writeToDom(newYorkBusinesses);
+
+
+const manufacturingBusinesses = businesses.filter(b => b.companyIndustry == "Manufacturing");
+
+writeToDom(manufacturingBusinesses);
